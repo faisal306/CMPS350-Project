b89ce93fbd6f52e62d0a4412cad5f3828c87d9fb
Yazan Jumaa <ys2110319@qu.edu.qa>
2025-04-03 21:08:30 +0300

    Add course data JSON and implement course loading functionality

--- Phase01/script/university.js
+++ Phase01/script/university.js
@@ -4,6 +4,7 @@
 // async used here because I am fetching the data from external source
 async function loadCourses() {
 
+
     // get the courses data
     const data = await fetch("../data/courses.json");
     const courses = await data.json();
@@ -15,13 +16,13 @@ async function loadCourses() {
     courses.forEach(course => {
         let courseItem = document.createElement('div');
         courseItem.classList.add('course-item');
-        courseItem.innerText = `
+        courseItem.innerHTML = `
 
             <h3>${course.name}</h3>
             <p>${course.description}</p>
             <p><strong>Instructor:</strong> ${course.instructor}</p>
             <p><strong>Schedule:</strong> ${course.schedule}</p>
-            <button onclick="registerCourse(${course.id})">Register Course</button>
+            <button onclick="registerCourse('${course.id}')">Register Course</button>
         `;
 
         // add this course to the container after you are done with adding the info of the course
@@ -35,8 +36,22 @@ function searchCourses() {
                                                               // In case the user used upper case
     let getFilter = document.getElementById("search-bar").value.toLowerCase();
                                                         // To get the courses from the container
+    // Because it will return an HTMLCollection it is just a special type of object
     let getCourses = document.getElementById("course-list").children;
 
-    
+    Array.from(getCourses).forEach(course => {
+        // Because every course has an h3 inside it that have the course name
+        // I used here 0 index because I do not need the whole collection
+        let courseName = course.getElementsByTagName("h3")[0].innerText.toLowerCase();
+        // Here I will get the input and then check if the course name contains the search term.
+        // if yes course remains, if no the course is hidden 
+        course.style.display = courseName.includes(getFilter) ? "block" : "none";
+    })
 
 }
+
+function registerSucc(courseId) {
+    alert(`You have registered for Course ID: ${courseId}`);
+}
+
+window.onload = loadCourses;
--- README.md
+++ README.md
@@ -7,6 +7,10 @@
 | Faisal Almalk | 202203280 |
 | Mohammed      | 202104361 |
 | Foaad         |  |
-| Yazan         | 202110319 |
+| Yazan Jumaa       | 202110319 |
 
 
+## Notes
+1. Course.json file
+   1. Just a simple array that will contain in it items objects.
+   2. Each object will be course with it details.
