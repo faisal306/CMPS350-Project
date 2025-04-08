db62fa5b26615bfcee1411c61d6502445d782e9d
Yazan Jumaa <ys2110319@qu.edu.qa>
2025-04-09 00:04:39 +0300

    Add getUserCourses method to categorize user courses by status

--- Phase01/app/repo/users-repo.js
+++ Phase01/app/repo/users-repo.js
@@ -33,7 +33,7 @@ class UsersRepo {
     }
 
 
-    // this method is to retrieve the user data by the Id
+    // this method just to retrieve the user data by the Id
     async getUser(id) {
         const users = await this.getUsers();
         // search for the user in the array 
@@ -44,7 +44,90 @@ class UsersRepo {
             return { errorMessage: 'User not found' };
     }
 
+
+    // this method will use the user Id to get his courses and categorizes them into
+    // completed / In-Progress / Pending 
+    async getUserCourses(userId) {
+
+
+        try {
+            // get user and course data
+            const users = await this.getUsers();
+            const user = users.find(u => u.id == userId);
+
+            if (!user) {
+                return { success: false, errorMessage: 'User not found' };
+            } 
+            
+
+            const allCourses = await fs.readJson(this.coursesFilePath);
+            
+            // Initialize categories
+            const completedCourses = user.completedCourses || [];
+            const registeredCourses = user.registeredCourses || [];
+            const pendingCourses = [];
+            const inProgressCourses = [];
+            
+            // loop through each course in the student's registeredCourses
+            for (const regCourse of registeredCourses) {
+
+                // get teh details 
+                const courseDetails = allCourses.find(c => c.crn == regCourse.id);
+
+                // if found
+                if (courseDetails) {
+
+                    // using the sperad i will combine regCourse and enrichedCourse
+                    // to get all the relevant info about the course and then categorize them
+                    const enrichedCourse = { ...regCourse, ...courseDetails };
+                    
+
+                    /*
+
+
+                    How the classification will be:
+                    pending: A course that the student has registered for but hasn't started yet. 
+                    It's still open for registration or is waiting to begin.
+
+                    in-Progress: A course that has already started, and the student 
+                    is currently attending. The course is ongoing but hasn't been completed yet.
+
+                    */
+
+                    // Check if the course is pending or in progress
+                    if (courseDetails.openForRegistration && !courseDetails.hasStarted) {
+                        // added to the pending courses array
+                        pendingCourses.push(enrichedCourse); 
+
+                        
+                    } else {
+                        inProgressCourses.push(enrichedCourse); 
+                    }
+                }
+            }
     
+            return {
+                success: true,
+                data: {
+                    // the list of courses
+                    completedCourses: completedCourses,
+                    inProgressCourses: inProgressCourses,
+                    pendingCourses: pendingCourses
+                }
+            };
+
+
+        // in case fail happend to when getting the courses 
+        } catch (error) {
+            console.error('Error getting user courses:', error);
+            return { 
+                success: false, errorMessage: 'An error occurred while retrieving user courses' 
+            };
+        }
+
+
+    }
+
 
 }
 
