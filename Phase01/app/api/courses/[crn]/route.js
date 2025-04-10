b97726aff338697890f8ac25510772aa87a5cfa5
Faisal-fa2203280 <faisalyousef2209@hotmail.com>
2025-04-10 14:30:21 +0300

    Dashboard fixes
    Register works but Unregister doesn't work yet

--- Phase01/app/api/courses/[crn]/route.js
+++ Phase01/app/api/courses/[crn]/route.js
@@ -10,10 +10,21 @@ export async function GET(request, { params }) {
 
 export async function POST(request, { params }) { //api/courses/[crn]
     try {
-        const { studentId, crn} = await request.json();
+        const { studentId, crn } = await request.json();
         console.log(studentId, crn);
         const registration = await CoursesRepo.registerClass(studentId, crn);
         return Response.json(registration);
+    } catch (error) {
+        return Response.json({ error: error.message }, { status: 400 });
+    }
+}
+
+export async function DELETE(request, { params }) {
+    try {
+        const { studentId } = await request.json();
+        const { crn } = params;
+        const result = await CoursesRepo.unregisterClass(studentId, crn);
+        return Response.json(result);
     } catch (error) {
         return Response.json({ error: error.message }, { status: 400 });
     }
